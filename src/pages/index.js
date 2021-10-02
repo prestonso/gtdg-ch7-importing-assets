96102d82996489c64be55012e61ddfef31114434
Preston So <pso@post.harvard.edu>
2021-10-02 14:52:04 -0300

    Add gatsby-astronaut.png to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
+// Indicate to Webpack that this module uses this image.
+import astronaut from "./gatsby-astronaut.png"
+
+console.log(astronaut)
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return <img src={astronaut} alt="The Gatsby astronaut" />
 }
